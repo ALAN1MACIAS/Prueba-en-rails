325b35c9fbb3f263eedeafd915f999851027a7f3
Alan Guillermo Avila Macias <a.g.a.m.3141335414@gmail.com>
2021-01-22 09:53:00 -0600

    Enviar datos al controlador

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,16 @@ class ArticlesController < ApplicationController
   end
   # get "/articles/:id"
   def show
-    @articles = Article.find(params[:id]) # params en un hash que lleva todos los paremetros que se enviaron al servidor web
+    @articles = Article.find(params[:id]) # params es un hash que lleva todos los paremetros que se enviaron al servidor web
   end
-  
+  # get "/articles/new"
+  def new
+    @articles = Article.new
+  end
+  # post "/articles"
+  def create
+    @articles = Article.new(title: params[:article][:title], body: params[:article][:body])
+    @articles.save
+    redirect_to @articles
+  end
 end
